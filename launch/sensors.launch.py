7bc01b30be0418ca308ef62667dabc160df7a3b0
RobotX-SMB <supermegabot@ethz-asl.ch>
2025-04-14 16:52:36 +0200

    tested rgb camera driver Jetson

--- launch/sensors.launch.py
+++ launch/sensors.launch.py
@@ -12,7 +12,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     # Declare all launch arguments
     declared_arguments = [
-        DeclareLaunchArgument("launch_lidar", default_value="true", description="Launch LiDAR"),
+        DeclareLaunchArgument("launch_lidar", default_value="false", description="Launch LiDAR"),
         DeclareLaunchArgument("launch_tracking_cam", default_value="false", description="Launch Tracking Camera"),
         DeclareLaunchArgument("launch_depth_cam", default_value="false", description="Launch Depth Camera"),
         # FIXME: launch_imu_interface needs rosserial_python, but it does not exist in ros2, need to find another option
@@ -24,80 +24,80 @@ def generate_launch_description():
         DeclareLaunchArgument("GPU_user", default_value="$(env USER)", description="Username to use on the Jetson Xavier GPU"),
     ]
 
-    # Robosense LiDAR
-    rslidar_config_file = get_package_share_directory('rslidar_sdk') + '/config/config.yaml'
-    lidar_group = GroupAction([
-        Node(
-            package="rslidar_sdk",
-            executable="rslidar_sdk_node",
-            name="rslidar_sdk_node",
-            output="screen",
-            parameters=[{'rslidar_config_path': rslidar_config_file}]
-        )
-    ], condition=IfCondition(LaunchConfiguration("launch_lidar")))
+    # # Robosense LiDAR
+    # rslidar_config_file = get_package_share_directory('rslidar_sdk') + '/config/config.yaml'
+    # lidar_group = GroupAction([
+    #     Node(
+    #         package="rslidar_sdk",
+    #         executable="rslidar_sdk_node",
+    #         name="rslidar_sdk_node",
+    #         output="screen",
+    #         parameters=[{'rslidar_config_path': rslidar_config_file}]
+    #     )
+    # ], condition=IfCondition(LaunchConfiguration("launch_lidar")))
 
-    # Intel RealSense Tracking Camera T265
-    tracking_camera_group = GroupAction([
-        Node(
-            package="realsense2_camera",
-            executable="realsense2_camera_node",
-            namespace="tracking_camera",
-            parameters=[{
-                "tracking_module.enable_mapping": False,
-                "tracking_module.enable_pose_jumping": False,
-                "tracking_module.enable_relocalization": False,
-                "device_type": "t265",
-                "enable_pose": True,
-                "enable_accel": True,
-                "enable_gyro": True,
-                "publish_tf": False,
-                "publish_odom_tf": False,
-                "topic_odom_in": "/control/smb_diff_drive/odom",
-                "unite_imu_method": "linear_interpolation",
-                "calib_odom_file": LaunchConfiguration("tracking_cam_calib_odom_file"),
-            }],
-            output="screen",
-        )
-    ], condition=IfCondition(LaunchConfiguration("launch_tracking_cam")))
+    # # Intel RealSense Tracking Camera T265
+    # tracking_camera_group = GroupAction([
+    #     Node(
+    #         package="realsense2_camera",
+    #         executable="realsense2_camera_node",
+    #         namespace="tracking_camera",
+    #         parameters=[{
+    #             "tracking_module.enable_mapping": False,
+    #             "tracking_module.enable_pose_jumping": False,
+    #             "tracking_module.enable_relocalization": False,
+    #             "device_type": "t265",
+    #             "enable_pose": True,
+    #             "enable_accel": True,
+    #             "enable_gyro": True,
+    #             "publish_tf": False,
+    #             "publish_odom_tf": False,
+    #             "topic_odom_in": "/control/smb_diff_drive/odom",
+    #             "unite_imu_method": "linear_interpolation",
+    #             "calib_odom_file": LaunchConfiguration("tracking_cam_calib_odom_file"),
+    #         }],
+    #         output="screen",
+    #     )
+    # ], condition=IfCondition(LaunchConfiguration("launch_tracking_cam")))
 
-    # Intel RealSense Depth Camera D435
-    depth_camera_group = GroupAction([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                PathJoinSubstitution([
-                    FindPackageShare("realsense2_camera"), "launch", "rs_launch.py"
-                ])
-            ),
-            launch_arguments={"camera": "depth_camera", "device_type": "d435"}.items(),
-        )
-    ], condition=IfCondition(LaunchConfiguration("launch_depth_cam")))
+    # # Intel RealSense Depth Camera D435
+    # depth_camera_group = GroupAction([
+    #     IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource(
+    #             PathJoinSubstitution([
+    #                 FindPackageShare("realsense2_camera"), "launch", "rs_launch.py"
+    #             ])
+    #         ),
+    #         launch_arguments={"camera": "depth_camera", "device_type": "d435"}.items(),
+    #     )
+    # ], condition=IfCondition(LaunchConfiguration("launch_depth_cam")))
 
-    # IMU Interface
-    imu_interface_group = GroupAction([
-        Node(
-            # FIXME:rosserial does not exist in ros2, need to find another option
-            package="rosserial_python",
-            executable="serial_node",
-            name="rosserial_python",
-            parameters=[{"_port": "/dev/versavis", "_baud": 250000}],
-            output="screen",
-            respawn=True,
-        ),
-        Node(
-            package="versavis_adis16448_receiver",
-            executable="versavis_imu_receiver",
-            name="versavis_imu_receiver",
-            parameters=[{
-                "imu_accelerator_sensitivity": 0.000833,
-                "imu_gyro_sensitivity": 0.04,
-                "imu_acceleration_covariance": 0.043864908,
-                "imu_gyro_covariance": 6e-9,
-                "imu_sub_topic": "/versavis/imu_micro",
-                "imu_pub_topic": "/imu",
-            }],
-            output="screen",
-        ),
-    ], condition=IfCondition(LaunchConfiguration("launch_imu_interface")))
+    # # IMU Interface
+    # imu_interface_group = GroupAction([
+    #     Node(
+    #         # FIXME:rosserial does not exist in ros2, need to find another option
+    #         package="rosserial_python",
+    #         executable="serial_node",
+    #         name="rosserial_python",
+    #         parameters=[{"_port": "/dev/versavis", "_baud": 250000}],
+    #         output="screen",
+    #         respawn=True,
+    #     ),
+    #     Node(
+    #         package="versavis_adis16448_receiver",
+    #         executable="versavis_imu_receiver",
+    #         name="versavis_imu_receiver",
+    #         parameters=[{
+    #             "imu_accelerator_sensitivity": 0.000833,
+    #             "imu_gyro_sensitivity": 0.04,
+    #             "imu_acceleration_covariance": 0.043864908,
+    #             "imu_gyro_covariance": 6e-9,
+    #             "imu_sub_topic": "/versavis/imu_micro",
+    #             "imu_pub_topic": "/imu",
+    #         }],
+    #         output="screen",
+    #     ),
+    # ], condition=IfCondition(LaunchConfiguration("launch_imu_interface")))
 
     # RGB Camera (FLIR Driver)
     # rgb_camera_group = GroupAction([
@@ -143,22 +143,25 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 PathJoinSubstitution([
-                    FindPackageShare("spinnaker_camera_driver"), "launch", "driver_node.launch.py"
+                    FindPackageShare("smb_bringup"), "launch", "rgb_camera_driver.launch.py"
                 ])
-            )
+            ),
+            launch_arguments={
+                'serial': "'20010195'" # for Jetson only
+            }.items()
         )
     ], condition=IfCondition(LaunchConfiguration("launch_rgb_cam")))
 
-    # Power Status
-    power_status_group = GroupAction([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                PathJoinSubstitution([
-                    FindPackageShare("smb_powerstatus"), "launch", "smb_powerstatus.launch.py"
-                ])
-            )
-        )
-    ], condition=IfCondition(LaunchConfiguration("launch_powerstatus")))
+    # # Power Status
+    # power_status_group = GroupAction([
+    #     IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource(
+    #             PathJoinSubstitution([
+    #                 FindPackageShare("smb_powerstatus"), "launch", "smb_powerstatus.launch.py"
+    #             ])
+    #         )
+    #     )
+    # ], condition=IfCondition(LaunchConfiguration("launch_powerstatus")))
 
         
     # Visualize in Rviz
@@ -176,12 +179,12 @@ def generate_launch_description():
     return LaunchDescription(
         declared_arguments +
         [
-            lidar_group,
-            tracking_camera_group,
-            depth_camera_group,
-            imu_interface_group,
+            # lidar_group,
+            # tracking_camera_group,
+            # depth_camera_group,
+            # imu_interface_group,
             rgb_camera_group,
-            power_status_group,
+            # power_status_group,
             rviz_group,
         ]
     )
