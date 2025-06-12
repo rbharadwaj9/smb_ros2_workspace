bc84326a93fe91220cc719bbcd00622692c894e8
RobotX-SMB <supermegabot@ethz-asl.ch>
2025-06-12 21:13:57 +0200

    Enhance debug launch file by adding robot state publisher and rslidar node, and updating robot description generation.

--- launch/debug_real.launch.py
+++ launch/debug_real.launch.py
@@ -1,13 +1,13 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, Command, FindExecutable
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import PythonExpression
 from ament_index_python.packages import get_package_share_directory
 import os
-
 
 
 def generate_launch_description():
@@ -38,6 +38,35 @@ def generate_launch_description():
         output="screen",
         parameters=[{"use_sim_time": False}],
     )
+
+    # Path to the URDF file
+    description_file = PathJoinSubstitution(
+        [FindPackageShare("smb_description"), "urdf", "smb.urdf.xacro"]
+    )
+
+    # Generate the robot description using xacro
+    robot_description = ParameterValue(
+        Command([FindExecutable(name="xacro"), " ", description_file]),
+        value_type=str
+    )
+
+    # Robot State Publisher node
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": robot_description, "use_sim_time": False}],    
+    )
+
+    rslidar_config_file = get_package_share_directory('smb_bringup') + '/config/rslidar_config.yaml'
+    rslidar = Node(
+            package="rslidar_sdk",
+            executable="rslidar_sdk_node",
+            name="rslidar_sdk_node",
+            output="screen",
+            parameters=[{'config_path': rslidar_config_file}]
+        )
 
     # Low-level gazebo controller node
     low_level_controller = Node(
@@ -185,20 +214,22 @@ def generate_launch_description():
     
     return LaunchDescription([
         # gazebo_launch,
+        robot_state_publisher_node,
+        rslidar,
         kinematics_controller,
         # low_level_controller,
         # joy_to_cmd_vel,
         # joy,
-        terrain_analysis,
-        terrain_analysis_ext,
+        # terrain_analysis,
+        # terrain_analysis_ext,
         # teleop_twist_joy_launch,
         dlio_launch,
-        local_odometry,
-        static_tf_map_to_odom,
-        far_planner_launch,
-        local_planner_launch,
-        twist_pid,
-        config_topics,
-        twist_mux,
-        rviz2,
+        # local_odometry,
+        # static_tf_map_to_odom,
+        # far_planner_launch,
+        # local_planner_launch,
+        # twist_pid,
+        # config_topics,
+        # twist_mux,
+        # rviz2,
     ])
