27d4d89b15e8787f926380d7b51feeeb8f745aa0
RobotX-SMB <supermegabot.smb261@gmail.com>
2025-06-21 01:45:21 +0200

    fix dds config

--- scripts/ros/smb_record.sh
+++ scripts/ros/smb_record.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # Determine workspace root from script location
 WORKSPACE_ROOT="$(cd "$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")" && pwd)"
