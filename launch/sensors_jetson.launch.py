b5d191f05c9f694cdae71c193b08d5a06ca74398
RobotX-SMB <supermegabot@ethz-asl.ch>
2025-06-24 06:08:02 +0200

    modified launch file with serial numbers

--- launch/sensors_jetson.launch.py
+++ launch/sensors_jetson.launch.py
@@ -1,15 +1,39 @@
+import os
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction, LogInfo
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node, ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    robot_id = os.environ.get('ROBOT_ID', '000')  # default to '000' if not set
+
+    # Mapping of robot_id to serial numbers
+    serial_map = {
+        '261': '20010195',
+        '262': '20023239',
+        '263': '20010198',
+        '264': '20023237',
+    }
+
+    # Handle missing or unrecognized robot_id
+    if robot_id == '000':
+        return LaunchDescription([
+            LogInfo(msg="Environment variable ROBOT_ID is not set or is default '000', exiting...")
+        ])
+
+    serial_ = serial_map.get(robot_id)
+    if serial_ is None:
+        return LaunchDescription([
+            LogInfo(msg=f"ROBOT_ID '{robot_id}' not recognized. No serial configured.")
+        ])
+
+    # Define the RGB camera group using the serial from the environment
     rgb_camera_group = GroupAction([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -18,11 +42,12 @@ def generate_launch_description():
                 ])
             ),
             launch_arguments={
-                'serial': "'20023237'" # for Jetson only
+                'serial': f"'{serial_}'"
             }.items()
         )
     ])
 
+    # Define the image processing container
     container = ComposableNodeContainer(
         name='image_proc_container',
         namespace='rgb_camera',
@@ -41,14 +66,12 @@ def generate_launch_description():
         ],
         output='screen',
         parameters=[
-                {"use_sim_time": False},
-            ],
+            {"use_sim_time": False},
+        ],
     )
 
-
-    return LaunchDescription(
-        [
-            rgb_camera_group,
-            container
-        ]
-    )
+    return LaunchDescription([
+        LogInfo(msg=f"Launching with ROBOT_ID: {robot_id} and serial: {serial_}"),
+        rgb_camera_group,
+        container
+    ])
